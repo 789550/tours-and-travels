bce416fcf839e5cd5ec9e52b89dc20dae08e101e
Sudipto Dutta <107313501+789550@users.noreply.github.com>
2024-07-24 11:19:45 +0530

    resolved main jsx change

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter basename='/tours-and-travels'>
       <App />
     </BrowserRouter>
