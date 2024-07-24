326e60e7803d3d982438e0893c3776bd44b90e68
Sudipto Dutta <107313501+789550@users.noreply.github.com>
2024-07-24 15:48:45 +0530

    resolving errors

--- src/pages/tours/TourList.jsx
+++ src/pages/tours/TourList.jsx
@@ -1,28 +1,37 @@
 
-import { Card, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import tours from "../../data/toursData";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import tours from '../../data/toursData';
 
 const TourList = () => {
   return (
-      <div className="tour-list bg-dark">
-        <h1 className="tour-list-heading">Explore Our Exciting Tours</h1>
-        <p className="tour-list-caption">Discover the world with our exclusive tour packages. Whether you're looking for adventure, relaxation, or cultural experiences, we have the perfect tour for you.</p>
-        <div className="card-container">
-          {tours.map(tour => (
-            <Card key={tour.id} className="card">
-              <Card.Img variant="top" src={tour.image} />
-              <Card.Body>
-                <Card.Title>{tour.name}</Card.Title>
-                <Card.Text>{tour.description}</Card.Text>
+    <div className="tour-list">
+      <h1 className="tour-list-heading">Explore Our Exciting Tours</h1>
+      <p className="tour-list-caption">Discover the world with our exclusive tour packages. Whether you're looking for adventure, relaxation, or cultural experiences, we have the perfect tour for you.</p>
+      <div className="card-container">
+        {tours.map(tour => (
+          <Card key={tour.id} className="card">
+            <Card.Img variant="top" src={tour.images[0]} />
+            <Card.Body>
+              <Card.Title>{tour.name}</Card.Title>
+              <Card.Text>{tour.description}</Card.Text>
+              <div className="tour-details">
+                <Card.Text><span style={{ color: "green" }} className="tour-price">Price: {tour.price}</span></Card.Text>
                 <Link to={`/tours/${tour.id}`}>
-                  <Button variant="dark">View Details</Button>
+                  <div className="view-details-button">   
+                    <FontAwesomeIcon icon={faEye} style={{ marginRight: '0.5em' }} />
+                    View Details
+                  </div>
                 </Link>
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
+              </div>
+            </Card.Body>
+          </Card>
+        ))}
       </div>
+    </div>
   );
 };
 
