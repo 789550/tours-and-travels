1e122a56f8ee56d006a08f559df9e7c530f0092e
Sudipto Dutta <107313501+789550@users.noreply.github.com>
2024-08-24 16:27:52 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
      <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter>
+    <BrowserRouter basename='/tours-and-travels/'>
       <App />
     </BrowserRouter>
     </PersistGate>
